f4d8afcb8f63473e0101b30d94da3f7f73735a9c
Nataniel Paiva <nataniel.paiva@gmail.com>
2024-06-18 20:41:15 -0300

    first commit

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+P
+ src/main/java/exemplos/Main.java,b/2/b24b9036b63def49f1a3cca6636596f985bc5cee
---
+++ src/main/java/exemplos/Main.java
@@ -0,0 +1,36 @@
+package exemplos;
+
+// Interface esperada pelo sistema
+interface Pagamento {
+    void pagar(double valor);
+}
+
+// Implementação da nova API de pagamento
+class PagamentoAPI {
+    void realizarPagamento(double valor) {
+        System.out.println("Pagamento de " + valor + " realizado com a nova API.");
+    }
+}
+
+// Adaptador que converte a interface da nova API para a interface esperada
+class PagamentoAdapter implements Pagamento {
+    private PagamentoAPI pagamentoAPI;
+
+    public PagamentoAdapter(PagamentoAPI pagamentoAPI) {
+        this.pagamentoAPI = pagamentoAPI;
+    }
+
+    @Override
+    public void pagar(double valor) {
+        pagamentoAPI.realizarPagamento(valor);
+    }
+}
+
+// Uso do adaptador
+public class Main {
+    public static void main(String[] args) {
+        PagamentoAPI apiNova = new PagamentoAPI();
+        Pagamento adaptador = new PagamentoAdapter(apiNova);
+        adaptador.pagar(100.0);
+    }
+}
